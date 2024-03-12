1e8335e1bccdc697f47262c4d5a26efd6c613333
OscarTenesaca <tenesaca.999@gmail.com>
2024-03-12 20:06:36 +0100

    change in home

--- lib/presentation/screens/widgets/weather_form.dart
+++ lib/presentation/screens/widgets/weather_form.dart
@@ -20,38 +20,44 @@ class _WeatherFormState extends State<WeatherForm> {
   Widget build(BuildContext context) {
     return Form(
       key: formKey,
-      child: TextFormField(
-        controller: _controller,
-        textAlign: TextAlign.center,
-        decoration: const InputDecoration(
-          hintText: 'Ciudad o lat y lng',
-        ),
-        onTapOutside: (event) => FocusScope.of(context).unfocus(),
-        onChanged: (value) {
-          setState(() {
-            city = value;
-          });
-        },
-        onSaved: (value) {
-          setState(() {
-            city = value!;
-          });
-        },
-        validator: (value) => value!.isEmpty
-            ? 'Por favor ingresa una ciudad o una lat y lng'
-            : null,
-        onEditingComplete: () async {
-          FocusScope.of(context).unfocus();
-          if (formKey.currentState!.validate()) {
-            formKey.currentState!.save();
-            final weatherProvider = context.read<WeatherRepositoryProvider>();
-            weatherProvider.error = '';
-            weatherProvider.isLoading = true;
-            await weatherProvider.getWeather(city: city);
-            weatherProvider.isLoading = false;
-            _controller.clear();
-          }
-        },
+      child: Wrap(
+        children: [
+          Text('Test'),
+          TextFormField(
+            controller: _controller,
+            textAlign: TextAlign.center,
+            decoration: const InputDecoration(
+              hintText: 'Ciudad o lat y lng',
+            ),
+            onTapOutside: (event) => FocusScope.of(context).unfocus(),
+            onChanged: (value) {
+              setState(() {
+                city = value;
+              });
+            },
+            onSaved: (value) {
+              setState(() {
+                city = value!;
+              });
+            },
+            validator: (value) => value!.isEmpty
+                ? 'Por favor ingresa una ciudad o una lat y lng'
+                : null,
+            onEditingComplete: () async {
+              FocusScope.of(context).unfocus();
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+                final weatherProvider =
+                    context.read<WeatherRepositoryProvider>();
+                weatherProvider.error = '';
+                weatherProvider.isLoading = true;
+                await weatherProvider.getWeather(city: city);
+                weatherProvider.isLoading = false;
+                _controller.clear();
+              }
+            },
+          ),
+        ],
       ),
     );
   }
